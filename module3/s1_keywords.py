fe6531f05a8da680a7a72fb69067311ea4022538
Ch. Katsidonis <ckatsidonis@gmail.com>
2020-11-09 11:14:37 +0100

    update

--- module3/s1_keywords.py
+++ module3/s1_keywords.py
@@ -6,11 +6,11 @@ import yake
 ignored = set(["conseil communal", "conseil général"])
 
 kw_extractor = yake.KeywordExtractor(lan="fr", top=20)
-data_path = "data/txt/"
+data_path = "../data/txt/"
 files = os.listdir(data_path)
 for f in sorted(files):
     if f.startswith("Bxl_"):
-        text = open(data_path + f).read()
+        text = open(data_path + f, encoding='latin-1').read()
         keywords = kw_extractor.extract_keywords(text)
         kept = []
         for score, kw in keywords:
